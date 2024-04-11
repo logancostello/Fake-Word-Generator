db7bbf6c9cc575b3d5a561f8f4ff6c55060783c1
Logan Costello <logancostello2022@gmail.com>
2024-04-10 18:35:29 -0700

    feat: func that counts frequencies of different letter pairs

--- fakewordgenerator.py
+++ fakewordgenerator.py
@@ -4,7 +4,29 @@ def load_words():
     return valid_words
 
 
+def create_empty_edge_list():
+    # create hashmap with english letters as keys (nodes in our graph)
+    letters = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+    graph = {letter: 0 for letter in letters + ['.']}
+
+    # set values to hashmap for tracking frequencies of following chars
+    freq = graph.copy()
+    for key in graph:
+        graph[key] = freq.copy()
+
+    return graph
+
+
+def count_frequencies(words, graph):
+    # read through all words and update frequencies
+    for word in words:
+        for i in range(len(word) - 1):
+            graph[word[i]][word[i + 1]] += 1
+        graph[word[-1]]['.'] += 1
+
+
 if __name__ == '__main__':
     english_words = load_words()
-    # demo print
-    print(english_words)
+    word_graph = create_empty_edge_list()
+    count_frequencies(english_words, word_graph)
+    print(word_graph['a']['.'])
