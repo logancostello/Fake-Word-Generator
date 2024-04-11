0d3e03072d7d9e9aca9e27ae89ae1af541819ed6
Logan Costello <logancostello2022@gmail.com>
2024-04-10 18:34:51 -0700

    feat: added unittests

---
+++ tests.py
@@ -0,0 +1,32 @@
+import unittest
+from fakewordgenerator import create_empty_edge_list,load_words, \
+    count_frequencies
+
+
+class TestCountFrequencies(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.english_words = load_words()
+        cls.graph = create_empty_edge_list()
+        count_frequencies(cls.english_words, cls.graph)
+
+    @classmethod
+    def tearDownClass(cls):
+        del cls.english_words
+        del cls.graph
+
+    def testPK(self): self.assertEqual(self.graph['p']['k'], 71)
+    def testQU(self): self.assertEqual(self.graph['q']['u'], 5763)
+    def testYY(self): self.assertEqual(self.graph['y']['y'], 9)
+    def testFL(self): self.assertEqual(self.graph['f']['l'], 4104)
+    def testEE(self): self.assertEqual(self.graph['e']['e'], 7312)
+    def testKL(self): self.assertEqual(self.graph['k']['l'], 1194)
+    def testLK(self): self.assertEqual(self.graph['l']['k'], 901)
+    def testZQ(self): self.assertEqual(self.graph['z']['q'], 4)
+    def testAA(self): self.assertEqual(self.graph['a']['a'], 255)
+    def testII(self): self.assertEqual(self.graph['i']['i'], 579)
+    def testGG(self): self.assertEqual(self.graph['g']['g'], 2113)
+
+
+
+
